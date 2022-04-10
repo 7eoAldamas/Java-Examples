92af71ab16685a4260c2e019a53fb167b6c4f874
7eo Aldamas <leo.aldamas@gmail.com>
2022-04-10 02:35:11 -0600

    First exercise - java

---
+++ src/exercise_1/Main.java
@@ -0,0 +1,43 @@
+package exercise_1;
+
+/**
+ * @author 7eoAldamas
+ *
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// numbers
+		byte num  = 120;
+		short num2 = 1200;
+		int num3 = 2000;
+		long num4 = 300000000;
+		
+		// float - double
+		float num5 = 12.0f;
+		double num6 = 3.141516;
+		
+		// boolean
+		boolean isFalse = true;
+		
+		// char
+		char caracter = 'A';
+		
+		// Objeto String
+		String name = "Leobardo Gonzalez Aldamas";
+		
+		// stdout
+		System.out.println("byte=> " + num + ", short=> " + num2);
+		System.out.println("int=> " + num3 + ", long=> " + num4);
+		System.out.println("float=> " + num5 + ", double=> " + num6);
+		
+		System.out.println("boolean=> " + isFalse);
+		
+		System.out.println("char=> " + caracter + ", String=> " + name);
+			
+	}
+
+}
